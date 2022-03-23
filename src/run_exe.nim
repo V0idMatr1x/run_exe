6b228ef40d9a387c89b439c75ec5d2dd7eb88e03
V0idMatr1x <chrispaige.dev@gmail.com>
2022-03-23 15:00:24 -0500

    pattern matching

--- src/run_exe.nim
+++ src/run_exe.nim
@@ -1,6 +1,6 @@
 import osproc
 import std / strformat
-
+import std / re
 
   #[Section: Executing Shell commands]#
 #__________________________________
@@ -9,6 +9,7 @@ import std / strformat
 proc `exe`*(cmd: string): int {.noSideEffect.} =
   execCmd cmd
 
+
 # Discard x because x is initialized but unused here.
 proc `run`*(cmd: int): proc {.noSideEffect.} =
   discard cmd
@@ -16,5 +17,13 @@ proc `run`*(cmd: int): proc {.noSideEffect.} =
 
   #[Section: String IO]#
 #__________________________________
+# echo a template string
+# Looks for `{}` in string
+# This is so the same oprator can be used interchangeably for regular strings and template strings
+# Limitation: only supports one string argument
+# For now echo must be used explicitly for multiple arguments & types
 proc `>>`*(strout: static string): proc =
-  echo fmt strout
+  if (r"^.{.}$" == strout):
+    echo fmt strout
+  else:
+    echo fmt strout
