f9748ae47859303c49a5c25504932f8146c423e7
V0idMatr1x <chrispaige.dev@gmail.com>
2022-03-23 21:59:55 -0500

    update

--- src/run_exe/bridge.nim
+++ src/run_exe/bridge.nim
@@ -1,10 +1,3 @@
-
-# This is just an example to get you started. Users of your library will
-# import this file by writing ``import run_exe/submodule``. Feel free to rename or
-# remove this file altogether. You may create additional modules alongside
-# this file as required.
-
-
 type
   bridge* = object
     name*: string
