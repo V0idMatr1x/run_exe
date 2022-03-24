77b58ff2c6f22522a4da68c779eb64543324c353
V0idMatr1x <chrispaige.dev@gmail.com>
2022-03-24 14:43:01 -0500

    update

--- src/run_exe/nmap.nim
+++ src/run_exe/nmap.nim
@@ -17,7 +17,7 @@ import run_exe
 
 proc `frag`(ip: string, port: string): proc =
   # Frag Scan
-  let frag_scan = fmt"sudo nmap -v -A -sC -sV -p {port} -f -Pn {ip}"
+  var frag_scan = "sudo nmap -v -A -sC -sV -p {port} -f -Pn {ip}"
   >> "Running Script!: {frag_scan}"
   return run exe frag_scan
 
