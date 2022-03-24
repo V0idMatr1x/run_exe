d2ceaab64cc6a042ac25763e08a7748d5d044e0c
V0idMatr1x <chrispaige.dev@gmail.com>
2022-03-23 22:00:55 -0500

    update

--- src/run_exe/sp3ctr3.nim
+++ src/run_exe/sp3ctr3.nim
@@ -11,7 +11,7 @@ import std/distros
 
 
 # Procedure that returns OS info
-proc os_info*(t: bool): string =
+proc os_eye*(t: bool): string =
     # If an OS is detected it will be returned as a string value
     if detectOs(ArchLinux) == t:
         return "Linux, Distro: ArchLinux"
