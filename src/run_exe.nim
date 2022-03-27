fba3f26083443c69a895b25e5b49ed447ece32f5
V0idMatr1x <88049272+V0idMatr1x@users.noreply.github.com>
2022-03-26 19:42:19 -0500

    Update run_exe.nim

--- src/run_exe.nim
+++ src/run_exe.nim
@@ -20,7 +20,7 @@ proc `run`*(cmd: int): proc {.noSideEffect.} =
 
   #[Section: String IO]#
 #__________________________________
-# @s Denotes a Template String STDOUT
+# >> Denotes a Template String STDOUT
 proc `>>`*(output: static string): proc =
   stdout.writeLine fmt output
 
