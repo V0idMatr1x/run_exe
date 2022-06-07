619f953f31a411891a92d2f31b8d79e93e5ab505
V0idMatr1x <88049272+V0idMatr1x@users.noreply.github.com>
2022-06-07 22:43:44 +0000

    Update libmath.nim

--- src/run_exe/libmath.nim
+++ src/run_exe/libmath.nim
@@ -8,7 +8,7 @@ proc initLibMath*(): libmath =
   libmath(name: "libmath")
 
 # Multiply 2 integers within a range of n
-func mul_rng*(a, b:int, n: int): int =
+func mul_rng*(a, b: int, n: int): int =
   func multiply(a, b: int): int =
     return a * b  
   return multiply(a, b) * n
