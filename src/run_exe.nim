495da5508d946ed75c7b1cc1cf4ac407eab8aefa
V0idMatr1x <v0idmatr1x.0@gmail.com>
2022-06-18 15:35:56 -0500

    update

--- src/run_exe.nim
+++ src/run_exe.nim
@@ -2,7 +2,6 @@ import osproc
 import std/browsers
 #[Base Utils]#
 
-
   #[Section: Executing CMD's in Shell]#
 #__________________________________
 
@@ -12,6 +11,8 @@ proc `exe`*(cmd: string): int =
 
 proc `run`*(cmd: int): proc {.noSideEffect.} =
   discard cmd
+
+#_______________________________________________
 
 # Sugar for opening a url with default browser 
 proc `>/`*(url: string): proc =
